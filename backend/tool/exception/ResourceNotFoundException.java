2ac8621106049563fcf7c2ed60f3af72447d6fe8
Arbiazabi07 <arbiazabi3094@gmail.com>
2026-04-22 13:23:11 +0530

    Day 3 - Service layer, Input validation and custom exceptions

---
+++ backend/tool/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package backend.tool.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " not found with id: " + id);
+    }
+}
